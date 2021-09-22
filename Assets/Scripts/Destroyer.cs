389dcc01057d37e99d3d28e8cc7761d56f56ef1b
Skicom123 <fajarrahmanarajab@gmail.com>
2021-09-22 15:56:43 +0700

    -

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    public GameController GameController;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        
+        string tag = col.gameObject.tag;
+        if (tag == "Bird" || tag == "Enemy" || tag == "Obstacle")
+        {
+            Destroy(col.gameObject);
+            GameController.GetComponent<GameController>().isDestroyed = true;
+            
+        }
+    }
+}
+
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public SlingShooter SlingShooter;
+    public List<Bird> Birds;
+    public List<Enemy> Enemies;
+    public TrailController TrailController;
+    public BoxCollider2D TapCollider;
+    private Bird _shotBird;
+    public YellowBird YellowBird;
+
+    public bool isDestroyed = false;
+
+
+    
+    private bool _isGameEnded = false;
+
+    void Start()
+    {
+        for(int i = 0; i < Birds.Count; i++)
+        {
+            Birds[i].OnBirdDestroyed += ChangeBird;
+            Birds[i].OnBirdShot += AssignTrail;
+        }
+
+        for(int i = 0; i < Enemies.Count; i++)
+        {
+            Enemies[i].OnEnemyDestroyed += CheckGameEnd;
+        }
+
+        TapCollider.enabled = false;
+        SlingShooter.InitiateBird(Birds[0]);
+        _shotBird = Birds[0];
+    }
+
+    public void ChangeBird()
+    {
+        TapCollider.enabled = false;
+
+        if (_isGameEnded)
+        {
+            return;
+        }
+
+        Birds.RemoveAt(0);
+
+        if(Birds.Count > 0)
+            SlingShooter.InitiateBird(Birds[0]);
+    }
+
+    public void AssignTrail(Bird bird)
+    {
+        TrailController.SetBird(bird);
+        StartCoroutine(TrailController.SpawnTrail());
+        TapCollider.enabled = true;
+    }
+
+    
+    public void CheckGameEnd(GameObject destroyedEnemy)
+    {
+        for(int i = 0; i < Enemies.Count; i++)
+        {
+            if(Enemies[i].gameObject == destroyedEnemy)
+            {
+                Enemies.RemoveAt(i);
+                break;
+            }
+        }
+
+        if(Enemies.Count == 0)
+        {
+            _isGameEnded = true;
+            SceneManager.LoadScene("Level 2");
+        }
+    }
+
+    void OnMouseUp()
+    {
+        if(_shotBird != null)
+        {
+            _shotBird.OnTap();
+        }
+    }
+
+    void Update() 
+    {
+        if (isDestroyed == false)
+        {
+             if (Input.GetMouseButton(0))
+             {
+                YellowBird.Boost();
+             }
+        }
+       else {
+
+       }
+    }
+
+    /*public void SetDestroy()
+    {
+        isDestroyed = true;
+    }
+    */
+}
